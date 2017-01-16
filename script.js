4616f226c2cd0ad915fbad68a65e6bfc976ff2f6
JL-golf14 <jeremy.leif@gmail.com>
2017-01-15 22:40:44 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function myname(){console.log("Jeremy Leif")};
 function bonus(){console.log("is trying to get the bonus")};
 function work(){console.log("does this work")};
-}
 
-myname(bonus(work()))
+
+myname(bonus(work()));
